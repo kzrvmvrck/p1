621188a7530259edc86d108fd81ad1893b2ae243
Keizarov <y.keizarov@gmail.com>
2024-11-30 22:42:42 +0300

    Add Swith operator

--- index.js
+++ index.js
@@ -135,4 +135,55 @@ for (let verifiableNumber = 2; verifiableNumber <= n; verifiableNumber++) {
     alert(verifiableNumber);
 }*/
 
+// Напишите if на swith
+/*
+switch (browser) {
+    case 'Edge':
+        alert( "You've got the Edge!" );
+        break;
 
+    case 'Chrome':
+    case 'Firefox':
+    case 'Safari':
+    case 'Opera':
+        alert( 'Okay we support these browsers too' );
+        break;
+
+    default:
+        alert( 'We hope that this page looks ok!' );
+}
+
+let browser;
+
+broeser == "Edge" ? "Use EDGE" :
+    browser == "chrome" || br == "Firefox" || browser == "Safari" || browser == "Opera" ? alert("OK") :
+    alert("Default browser");*/
+
+// Переписать условие if на switch
+
+const number = +prompt('Введите число между 0 и 3', '');
+
+if (number === 0) {
+    alert('Вы ввели число 0');
+}
+
+if (number === 1) {
+    alert('Вы ввели число 1');
+}
+
+if (number === 2 || number === 3) {
+    alert('Вы ввели число 2, а может и 3');
+}
+
+switch (number) {
+    case 0:
+        alert("0");
+        break;
+    case 1:
+        alert("1");
+        break;
+    case 2:
+    case 3:
+        alert("2 or 3");
+        break;
+}
